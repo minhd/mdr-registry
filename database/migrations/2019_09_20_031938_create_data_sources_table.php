2f680dadf6d8e4bcee4dc29c68f497c0523e9bd8
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2019-09-20 13:29:05 +1000

    DataSource model creation

---
+++ database/migrations/2019_09_20_031938_create_data_sources_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataSourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_sources', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_sources');
+    }
+}
---
+++ tests/Unit/DataSourceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\DataSource;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DataSourceTest extends TestCase
+{
+    use DatabaseMigrations, RefreshDatabase;
+
+    /** @test */
+    function a_datasource_has_an_owner()
+    {
+        $john = create(User::class);
+        $ds = create(DataSource::class, ['user_id' => $john->id]);
+
+        $this->assertEquals($ds->owner->id, $john->id);
+    }
+}
