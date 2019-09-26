b18e90c8f5c8e9bbe9e2210b1eb08165d1f36874
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2019-09-26 10:36:29 +1000

    Fixed an issue with logging in

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Registry\Models\User::class,
         ],
 
         // 'users' => [
