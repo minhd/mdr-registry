6558e2e77397698f8fd1ac0a9fd061b23830e2ac
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2019-09-27 12:39:12 +1000

    VueSPA starting up

---
+++ resources/js/components/RegistryDashboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-full">
+        <nav class="bg-white px-16 py-8 flex justify-between border-b border-gray-300">
+            <a href="">Dashboard</a>
+            <a href="">IGSN</a>
+            <a href="">Data Sources</a>
+            <a href="" @click.prevent="logout">
+                Logout
+            </a>
+        </nav>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'RegistryDashboard',
+    methods: {
+      logout () {
+        window.axios.post('/logout').then(response => {
+          window.location = '/login'
+        });
+      }
+    },
+    mounted() {
+      // window.axios.get('/api/me')
+      //   .then(response => {
+      //     window.location(response.headers.location)
+      //   });
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,5 @@
 @extends('layouts.single')
 
 @section('content')
-    <div class="p-10 bg-gray-200 w-full h-screen flex flex-wrap flex-col content-center m-auto justify-center">
-        <div class="bg-white w-full lg:w-1/3 px-6 py-16 border border-gray-300 rounded-lg py-2 px-4 block w-1/2">
-            You are logged in!
-
-            <div class="" aria-labelledby="navbarDropdown">
-                <a class="" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                    @csrf
-                </form>
-            </div>
-        </div>
-    </div>
+    <registry-dashboard></registry-dashboard>
 @endsection
